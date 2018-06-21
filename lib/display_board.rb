39b7bc2234849b232497ddbdd87445f8a8e2d0a1
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-21 06:56:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 board = [ " ", " ", " ", " ", " ", " ", " ", " ", " " ]
 def display_board(board)
-  puts "   |   |   "
+  puts "#{board}|#{board}|#{board}"
   puts "-----------"
-  puts "   |   |   "
+  puts "#{board}|#{board}|#{board}"
   puts "-----------"
-  puts "   |   |   "
+  puts "#{board}|#{board}|#{board}"
 end
 # out the current state.
 
